1fdf3f2137ce496969c2a7571790332273e002e7
Michael Pitzer <mail@mpitzer.at>
2016-09-18 04:56:27 +0200

    Debounce renderEvents()

--- src/defaults.js
+++ src/defaults.js
@@ -91,8 +91,9 @@ Calendar.defaults = {
 	handleWindowResize: true,
 	windowResizeDelay: 100, // milliseconds before an updateSize happens
 
-	longPressDelay: 1000
+	longPressDelay: 1000,
 	
+	renderDelay: 0 // calendar render debounce timeout
 };
 
 
